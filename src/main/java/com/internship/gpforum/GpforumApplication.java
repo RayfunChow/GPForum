43f846a31b5b1f0a79855fb58c96c6efc6797cd4
Rayfun <a648186412@gmail.com>
2019-07-13 11:05:57 +0800

    first commit

---
+++ src/main/java/com/internship/gpforum/GpforumApplication.java
@@ -0,0 +1,13 @@
+package com.internship.gpforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpforumApplication.class, args);
+    }
+
+}
